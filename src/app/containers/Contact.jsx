73e986fec30d202e075f2484ebbbccfd703e571f
Kaveh Karami <kaveh.karami.kk@gmail.com>
2020-10-07 19:31:56 +0330

    wip - reafactor Contact Container : google map  Section

--- src/app/containers/Contact.jsx
+++ src/app/containers/Contact.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import GoogleMap from '../components/contact/GoogleMap';
 import Information from '../components/contact/Information';
 import Breadcrumb from "../components/shared/sections/Beadcrumb"
 
@@ -6,7 +7,6 @@ const Contact = () => {
   return (
     <main id="main">
       <Breadcrumb name="Contact" />
-
 
       <section class="contact" data-aos="fade-up" data-aos-easing="ease-in-out" data-aos-duration="500">
         <div class="container">
@@ -54,13 +54,7 @@ const Contact = () => {
         </div>
       </section>
 
-
-      <section class="map mt-2">
-        <div class="container-fluid p-0">
-          <iframe title="google map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3024.2219901290355!2d-74.00369368400567!3d40.71312937933185!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c25a23e28c1191%3A0x49f75d3281df052a!2s150%20Park%20Row%2C%20New%20York%2C%20NY%2010007%2C%20USA!5e0!3m2!1sen!2sbg!4v1579767901424!5m2!1sen!2sbg" frameborder="0" style={{ border: 0 }} allowfullscreen=""></iframe>
-        </div>
-      </section>
-
+      <GoogleMap />
     </main >
 
   );
