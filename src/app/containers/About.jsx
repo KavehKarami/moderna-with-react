c4cdc14daf1fe558f1488aac378ad36b16e43284
Kaveh Karami <kaveh.karami.kk@gmail.com>
2020-10-07 16:27:34 +0330

    refactor About Container

--- src/app/containers/About.jsx
+++ src/app/containers/About.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import AboutCompany from '../components/about/AboutCompany';
 import Fact from '../components/about/fact/Fact';
 import Skills from '../components/about/skills/Skills';
+import Testimonilas from '../components/about/testimonials/Testimonilas';
 import Breadcrumb from '../components/shared/sections/Beadcrumb';
 
 const About = () => {
@@ -15,75 +16,7 @@ const About = () => {
 
       <Skills />
 
-      <section className="testimonials" data-aos="fade-up">
-        <div className="container">
-
-          <div className="section-title">
-            <h2>Tetstimonials</h2>
-            <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
-          </div>
-
-          <div className="owl-carousel testimonials-carousel">
-
-            <div className="testimonial-item">
-              <img src={require("../../assets/img/testimonials/testimonials-1.jpg")} className="testimonial-img" alt="" />
-              <h3>Saul Goodman</h3>
-              <h4>Ceo &amp; Founder</h4>
-              <p>
-                <i className="bx bxs-quote-alt-left quote-icon-left"></i>
-              Proin iaculis purus consequat sem cure digni ssim donec porttitora entum suscipit rhoncus. Accusantium quam, ultricies eget id, aliquam eget nibh et. Maecen aliquam, risus at semper.
-              <i className="bx bxs-quote-alt-right quote-icon-right"></i>
-              </p>
-            </div>
-
-            <div className="testimonial-item">
-              <img src={require("../../assets/img/testimonials/testimonials-2.jpg")} className="testimonial-img" alt="" />
-              <h3>Sara Wilsson</h3>
-              <h4>Designer</h4>
-              <p>
-                <i className="bx bxs-quote-alt-left quote-icon-left"></i>
-              Export tempor illum tamen malis malis eram quae irure esse labore quem cillum quid cillum eram malis quorum velit fore eram velit sunt aliqua noster fugiat irure amet legam anim culpa.
-              <i className="bx bxs-quote-alt-right quote-icon-right"></i>
-              </p>
-            </div>
-
-            <div className="testimonial-item">
-              <img src={require("../../assets/img/testimonials/testimonials-3.jpg")} className="testimonial-img" alt="" />
-              <h3>Jena Karlis</h3>
-              <h4>Store Owner</h4>
-              <p>
-                <i className="bx bxs-quote-alt-left quote-icon-left"></i>
-              Enim nisi quem export duis labore cillum quae magna enim sint quorum nulla quem veniam duis minim tempor labore quem eram duis noster aute amet eram fore quis sint minim.
-              <i className="bx bxs-quote-alt-right quote-icon-right"></i>
-              </p>
-            </div>
-
-            <div className="testimonial-item">
-              <img src={require("../../assets/img/testimonials/testimonials-4.jpg")} className="testimonial-img" alt="" />
-              <h3>Matt Brandon</h3>
-              <h4>Freelancer</h4>
-              <p>
-                <i className="bx bxs-quote-alt-left quote-icon-left"></i>
-              Fugiat enim eram quae cillum dolore dolor amet nulla culpa multos export minim fugiat minim velit minim dolor enim duis veniam ipsum anim magna sunt elit fore quem dolore labore illum veniam.
-              <i className="bx bxs-quote-alt-right quote-icon-right"></i>
-              </p>
-            </div>
-
-            <div className="testimonial-item">
-              <img src={require("../../assets/img/testimonials/testimonials-5.jpg")} className="testimonial-img" alt="" />
-              <h3>John Larson</h3>
-              <h4>Entrepreneur</h4>
-              <p>
-                <i className="bx bxs-quote-alt-left quote-icon-left"></i>
-              Quis quorum aliqua sint quem legam fore sunt eram irure aliqua veniam tempor noster veniam enim culpa labore duis sunt culpa nulla illum cillum fugiat legam esse veniam culpa fore nisi cillum quid.
-              <i className="bx bxs-quote-alt-right quote-icon-right"></i>
-              </p>
-            </div>
-
-          </div>
-
-        </div>
-      </section>
+      <Testimonilas />
 
     </main>
   );
