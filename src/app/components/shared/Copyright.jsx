5e843773e624413bbe6347e661f6590691e9146f
Kaveh Karami <kaveh.karami.kk@gmail.com>
2020-10-07 12:21:39 +0330

    implement Copyright Component

---
+++ src/app/components/shared/Copyright.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Copyright = () => {
+  return (
+    <div className="container">
+      <div className="copyright">
+        &copy; Copyright <strong><span>Moderna</span></strong>. All Rights Reserved
+        </div>
+      <div className="credits">
+        Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+      </div>
+    </div>
+  );
+}
+
+export default Copyright;
