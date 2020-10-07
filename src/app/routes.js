dfd27d4167eb81c0ae354b5d01c08d1caf8b261d
Kaveh Karami <kaveh.karami.kk@gmail.com>
2020-10-07 11:36:17 +0330

    add some routes

--- src/app/routes.js
+++ src/app/routes.js
@@ -3,5 +3,10 @@ import About from "./containers/About";
 
 export const routes = [
   { path: "/", component: Home, exact: true, label: "Home" },
-  { path: "/about", component: About, exact: true, label: "About" },
+  { path: "/about", component: About, exact: true, label: "About Us" },
+  { path: "/services", component: Services, exact: true, label: "Services" },
+  { path: "/portfolio", component: Portfolio, exact: true, label: "Portfolio" },
+  { path: "/team", component: Team, exact: true, label: "Team" },
+  { path: "/blog", component: Blog, exact: true, label: "Blog" },
+  { path: "/contact", Component: contact, exact: true, label: "Contact Us" },
 ];
