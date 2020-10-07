84d7dbd782e48bcc10285cdd033611c9820404dc
Kaveh Karami <kaveh.karami.kk@gmail.com>
2020-10-07 18:58:28 +0330

    fix some issues

--- src/app/components/team/TeamItem.jsx
+++ src/app/components/team/TeamItem.jsx
@@ -2,18 +2,18 @@ import React from 'react';
 
 const TeamItem = (props) => {
   return (
-    <div class="col-lg-4 col-md-6 d-flex align-items-stretch">
-      <div class="member">
-        <div class="member-img">
-          <img src={props.imgURL} class="img-fluid" alt="" />
-          <div class="social">
-            <a href=""><i class="icofont-twitter"></i></a>
-            <a href=""><i class="icofont-facebook"></i></a>
-            <a href=""><i class="icofont-instagram"></i></a>
-            <a href=""><i class="icofont-linkedin"></i></a>
+    <div className="col-lg-4 col-md-6 d-flex align-items-stretch">
+      <div className="member">
+        <div className="member-img">
+          <img src={props.imgURL} className="img-fluid" alt="" />
+          <div className="social">
+            <a href="/team/#"><i className="icofont-twitter"></i></a>
+            <a href="/team/#"><i className="icofont-facebook"></i></a>
+            <a href="/team/#"><i className="icofont-instagram"></i></a>
+            <a href="/team/#"><i className="icofont-linkedin"></i></a>
           </div>
         </div>
-        <div class="member-info">
+        <div className="member-info">
           <h4>{props.person}</h4>
           <span>{props.job}</span>
           <p>{props.description}</p>
