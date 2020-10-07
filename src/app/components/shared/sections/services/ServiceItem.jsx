5dcd909fba2cb869e4067ab2888037047147cd7b
Kaveh Karami <kaveh.karami.kk@gmail.com>
2020-10-07 13:34:01 +0330

    update ServiceItem Component

--- src/app/components/shared/sections/services/ServiceItem.jsx
+++ src/app/components/shared/sections/services/ServiceItem.jsx
@@ -1,13 +1,10 @@
 import React from 'react';
+import IconBox from '../../IconBox';
 
 const ServiceItem = (props) => {
   return (
     <div className="col-md-6 col-lg-3 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay={props.delay || "0"}>
-      <div className={`icon-box ${props.iconBoxClass}`}>
-        <div className="icon"><i className={props.iconClass}></i></div>
-        <h4 className="title"><a href="/">{props.title}</a></h4>
-        <p className="description">{props.description}</p>
-      </div>
+      <IconBox iconBoxClass={props.iconBoxClass} iconClass={props.iconClass} title={props.title} description={props.description} />
     </div>
   );
 }
