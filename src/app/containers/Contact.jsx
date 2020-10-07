9dae635b1f66321838dc266e2a7502f4ec708a8c
Kaveh Karami <kaveh.karami.kk@gmail.com>
2020-10-07 19:28:21 +0330

    wip - reafactor Contact Container : Information Section

--- src/app/containers/Contact.jsx
+++ src/app/containers/Contact.jsx
@@ -1,56 +1,23 @@
 import React from 'react';
+import Information from '../components/contact/Information';
+import Breadcrumb from "../components/shared/sections/Beadcrumb"
 
 const Contact = () => {
   return (
     <main id="main">
-
-
-      <section class="breadcrumbs">
-        <div class="container">
-
-          <div class="d-flex justify-content-between align-items-center">
-            <h2>Contact</h2>
-            <ol>
-              <li><a href="index.html">Home</a></li>
-              <li>Contact</li>
-            </ol>
-          </div>
-
-        </div>
-      </section>
+      <Breadcrumb name="Contact" />
 
 
       <section class="contact" data-aos="fade-up" data-aos-easing="ease-in-out" data-aos-duration="500">
         <div class="container">
-
           <div class="row">
 
             <div class="col-lg-6">
-
               <div class="row">
-                <div class="col-md-12">
-                  <div class="info-box">
-                    <i class="bx bx-map"></i>
-                    <h3>Our Address</h3>
-                    <p>A108 Adam Street, New York, NY 535022</p>
-                  </div>
-                </div>
-                <div class="col-md-6">
-                  <div class="info-box">
-                    <i class="bx bx-envelope"></i>
-                    <h3>Email Us</h3>
-                    <p>info@example.com<br />contact@example.com</p>
-                  </div>
-                </div>
-                <div class="col-md-6">
-                  <div class="info-box">
-                    <i class="bx bx-phone-call"></i>
-                    <h3>Call Us</h3>
-                    <p>+1 5589 55488 55<br />+1 6678 254445 41</p>
-                  </div>
-                </div>
+                <Information className="col-md-12" classIcon="bx-map" title="Our Address" text={<p>A108 Adam Street, New York, NY 535022</p>} />
+                <Information classIcon="bx-envelope" title="Email Us" text={<p>info@example.com<br />contact@example.com</p>} />
+                <Information classIcon="bx-phone-call" title="Call Us" text={<p>+1 5589 55488 55<br />+1 6678 254445 41</p>} />
               </div>
-
             </div>
 
             <div class="col-lg-6">
