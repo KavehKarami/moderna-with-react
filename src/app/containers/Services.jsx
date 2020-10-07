bead01449217f383ae08f745b44308eeacdf2b7f
Kaveh Karami <kaveh.karami.kk@gmail.com>
2020-10-07 17:14:33 +0330

    wip - refactor Service container

--- src/app/containers/Services.jsx
+++ src/app/containers/Services.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import ServiceDetails from '../components/services/ServiceDetails';
 import Breadcrumb from "../components/shared/sections/Beadcrumb"
 import OurServices from "../components/shared/sections/services/OurServices"
 import WhyUs from '../components/shared/sections/whyUs/WhyUs';
@@ -13,112 +14,55 @@ const Services = () => {
 
       <WhyUs />
 
-      <section class="service-details">
-        <div class="container">
+      <ServiceDetails />
 
-          <div class="row">
-            <div class="col-md-6 d-flex align-items-stretch" data-aos="fade-up">
-              <div class="card">
-                <div class="card-img">
-                  <img src="assets/img/service-details-1.jpg" alt="..." />
-                </div>
-                <div class="card-body">
-                  <h5 class="card-title"><a href="#">Our Mission</a></h5>
-                  <p class="card-text">Lorem ipsum dolor sit amet, consectetur elit, sed do eiusmod tempor ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat</p>
-                  <div class="read-more"><a href="#"><i class="icofont-arrow-right"></i> Read More</a></div>
-                </div>
-              </div>
-            </div>
-            <div class="col-md-6 d-flex align-items-stretch" data-aos="fade-up">
-              <div class="card">
-                <div class="card-img">
-                  <img src="assets/img/service-details-2.jpg" alt="..." />
-                </div>
-                <div class="card-body">
-                  <h5 class="card-title"><a href="#">Our Plan</a></h5>
-                  <p class="card-text">Sed ut perspiciatis unde omnis iste natus error sit voluptatem doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo</p>
-                  <div class="read-more"><a href="#"><i class="icofont-arrow-right"></i> Read More</a></div>
-                </div>
-              </div>
+      <section className="pricing section-bg" data-aos="fade-up">
+        <div className="container">
 
-            </div>
-            <div class="col-md-6 d-flex align-items-stretch" data-aos="fade-up">
-              <div class="card">
-                <div class="card-img">
-                  <img src="assets/img/service-details-3.jpg" alt="..." />
-                </div>
-                <div class="card-body">
-                  <h5 class="card-title"><a href="#">Our Vision</a></h5>
-                  <p class="card-text">Nemo enim ipsam voluptatem quia voluptas sit aut odit aut fugit, sed quia magni dolores eos qui ratione voluptatem sequi nesciunt Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet</p>
-                  <div class="read-more"><a href="#"><i class="icofont-arrow-right"></i> Read More</a></div>
-                </div>
-              </div>
-            </div>
-            <div class="col-md-6 d-flex align-items-stretch" data-aos="fade-up">
-              <div class="card">
-                <div class="card-img">
-                  <img src="assets/img/service-details-4.jpg" alt="..." />
-                </div>
-                <div class="card-body">
-                  <h5 class="card-title"><a href="#">Our Care</a></h5>
-                  <p class="card-text">Nostrum eum sed et autem dolorum perspiciatis. Magni porro quisquam laudantium voluptatem. In molestiae earum ab sit esse voluptatem. Eos ipsam cumque ipsum officiis qui nihil aut incidunt aut</p>
-                  <div class="read-more"><a href="#"><i class="icofont-arrow-right"></i> Read More</a></div>
-                </div>
-              </div>
-            </div>
-          </div>
-
-        </div>
-      </section>
-
-
-      <section class="pricing section-bg" data-aos="fade-up">
-        <div class="container">
-
-          <div class="section-title">
+          <div className="section-title">
             <h2>Pricing</h2>
             <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
           </div>
 
-          <div class="row no-gutters">
+          <div className="row no-gutters">
 
-            <div class="col-lg-4 box">
+            <div className="col-lg-4 box">
               <h3>Free</h3>
               <h4>$0<span>per month</span></h4>
               <ul>
-                <li><i class="bx bx-check"></i> Quam adipiscing vitae proin</li>
-                <li><i class="bx bx-check"></i> Nec feugiat nisl pretium</li>
-                <li><i class="bx bx-check"></i> Nulla at volutpat diam uteera</li>
-                <li class="na"><i class="bx bx-x"></i> <span>Pharetra massa massa ultricies</span></li>
-                <li class="na"><i class="bx bx-x"></i> <span>Massa ultricies mi quis hendrerit</span></li>
+                <li><i className="bx bx-check"></i> Quam adipiscing vitae proin</li>
+                <li><i className="bx bx-check"></i> Nec feugiat nisl pretium</li>
+                <li><i className="bx bx-check"></i> Nulla at volutpat diam uteera</li>
+                <li className="na"><i className="bx bx-x"></i> <span>Pharetra massa massa ultricies</span></li>
+                <li className="na"><i className="bx bx-x"></i> <span>Massa ultricies mi quis hendrerit</span></li>
               </ul>
-              <a href="#" class="get-started-btn">Get Started</a>
+              <a href="/services/#" className="get-started-btn">Get Started</a>
             </div>
 
-            <div class="col-lg-4 box featured">
+            <div className="col-lg-4 box featured">
               <h3>Business</h3>
               <h4>$29<span>per month</span></h4>
               <ul>
-                <li><i class="bx bx-check"></i> Quam adipiscing vitae proin</li>
-                <li><i class="bx bx-check"></i> Nec feugiat nisl pretium</li>
-                <li><i class="bx bx-check"></i> Nulla at volutpat diam uteera</li>
-                <li><i class="bx bx-check"></i> Pharetra massa massa ultricies</li>
-                <li><i class="bx bx-check"></i> Massa ultricies mi quis hendrerit</li>
+                <li><i className="bx bx-check"></i> Quam adipiscing vitae proin</li>
+                <li><i className="bx bx-check"></i> Nec feugiat nisl pretium</li>
+                <li><i className="bx bx-check"></i> Nulla at volutpat diam uteera</li>
+                <li><i className="bx bx-check"></i> Pharetra massa massa ultricies</li>
+                <li><i className="bx bx-check"></i> Massa ultricies mi quis hendrerit</li>
               </ul>
-              <a href="#" class="get-started-btn">Get Started</a>
+              <a href="/services/#" className="get-started-btn">Get Started</a>
             </div>
 
-            <div class="col-lg-4 box">
+            <div className="col-lg-4 box">
               <h3>Developer</h3>
               <h4>$49<span>per month</span></h4>
               <ul>
-                <li><i class="bx bx-check"></i> Quam adipiscing vitae proin</li>
-                <li><i class="bx bx-check"></i> Nec feugiat nisl pretium</li>
-                <li><i class="bx bx-check"></i> Nulla at volutpat diam uteera</li>
-                <li><i class="bx bx-check"></i> Pharetra massa massa ultricies</li>
-                <li><i class="bx bx-check"></i> Massa ultricies mi quis hendrerit</li>
+                <li><i className="bx bx-check"></i> Quam adipiscing vitae proin</li>
+                <li><i className="bx bx-check"></i> Nec feugiat nisl pretium</li>
+                <li><i className="bx bx-check"></i> Nulla at volutpat diam uteera</li>
+                <li><i className="bx bx-check"></i> Pharetra massa massa ultricies</li>
+                <li><i className="bx bx-check"></i> Massa ultricies mi quis hendrerit</li>
               </ul>
-              <a href="#" class="get-started-btn">Get Started</a>
+              <a href="/services/#" className="get-started-btn">Get Started</a>
             </div>
 
           </div>
