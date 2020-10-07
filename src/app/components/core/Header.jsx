e2a109bc3d7f4f4e1bd2d095865762c9c0c4822c
Kaveh Karami <kaveh.karami.kk@gmail.com>
2020-10-07 11:44:09 +0330

    implement Header Component

--- src/app/components/core/Header.jsx
+++ src/app/components/core/Header.jsx
@@ -4,9 +4,43 @@ import CustomLink from '../utils/CustomLink';
 
 const Footer = () => {
   return (
-    <div>
-      {routes.map(((route, index) => <CustomLink key={index} path={route.path} exact={route.exact} label={route.label} />))}
-    </div>
+    <React.Fragment>
+      <header id="header" className="fixed-top header-transparent">
+        <div className="container">
+
+          <div className="logo float-left">
+            <h1 className="text-light"><a href="index.html"><span>Moderna</span></a></h1>
+          </div>
+
+          <nav className="nav-menu  float-right d-none d-lg-block">
+            <ul>
+              {routes.map(((route, index) => <CustomLink key={index} path={route.path} exact={route.exact} label={route.label} />))}
+              <li className="drop-down"><a href="/">Drop Down</a>
+                <ul>
+                  <li><a href="/#">Drop Down 1</a></li>
+                  <li className="drop-down"><a href="/#">Drop Down 2</a>
+                    <ul>
+                      <li><a href="/#">Deep Drop Down 1</a></li>
+                      <li><a href="/#">Deep Drop Down 2</a></li>
+                      <li><a href="/#">Deep Drop Down 3</a></li>
+                      <li><a href="/#">Deep Drop Down 4</a></li>
+                      <li><a href="/#">Deep Drop Down 5</a></li>
+                    </ul>
+                  </li>
+                  <li><a href="/#">Drop Down 3</a></li>
+                  <li><a href="/#">Drop Down 4</a></li>
+                  <li><a href="/#">Drop Down 5</a></li>
+                </ul>
+              </li>
+            </ul>
+          </nav>
+
+
+        </div>
+      </header>
+
+
+    </React.Fragment>
   );
 }
 
