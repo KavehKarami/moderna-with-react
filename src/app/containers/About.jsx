60429d1a310fa577e6b0573d6063a6ae5f3725f2
Kaveh Karami <kaveh.karami.kk@gmail.com>
2020-10-07 15:43:13 +0330

    wip - refactor About container

--- src/app/containers/About.jsx
+++ src/app/containers/About.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Fact from '../components/about/fact/Fact';
 import Breadcrumb from '../components/shared/sections/Beadcrumb';
 
 const About = () => {
@@ -36,35 +37,7 @@ const About = () => {
       </section>
 
 
-      <section className="facts section-bg" data-aos="fade-up">
-        <div className="container">
-
-          <div className="row counters">
-
-            <div className="col-lg-3 col-6 text-center">
-              <span data-toggle="counter-up">232</span>
-              <p>Clients</p>
-            </div>
-
-            <div className="col-lg-3 col-6 text-center">
-              <span data-toggle="counter-up">521</span>
-              <p>Projects</p>
-            </div>
-
-            <div className="col-lg-3 col-6 text-center">
-              <span data-toggle="counter-up">1,463</span>
-              <p>Hours Of Support</p>
-            </div>
-
-            <div className="col-lg-3 col-6 text-center">
-              <span data-toggle="counter-up">15</span>
-              <p>Hard Workers</p>
-            </div>
-
-          </div>
-
-        </div>
-      </section>
+      <Fact />
 
 
       <section className="skills" data-aos="fade-up">
