5c01aeb951949153ecc6298d6cf8ad54f60b6ac7
Kaveh Karami <kaveh.karami.kk@gmail.com>
2020-10-11 17:02:16 +0330

    update routes.js

--- src/app/routes.js
+++ src/app/routes.js
@@ -4,6 +4,7 @@ import Services from "./containers/Services";
 import Portfolio from "./containers/Portfolio";
 import Team from "./containers/Team";
 import Blog from "./containers/Blog";
+import BlogSingle from "./containers/BlogSingle";
 import Contact from "./containers/Contact";
 
 export const routes = [
@@ -13,5 +14,6 @@ export const routes = [
   { path: "/portfolio", component: Portfolio, exact: true, label: "Portfolio" },
   { path: "/team", component: Team, exact: true, label: "Team" },
   { path: "/blog", component: Blog, exact: true, label: "Blog" },
+  { path: "/blog/:id", component: BlogSingle, exact: true },
   { path: "/contact", component: Contact, exact: true, label: "Contact Us" },
 ];
