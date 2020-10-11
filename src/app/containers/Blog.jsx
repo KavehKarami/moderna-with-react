5b7fd8e357795090aed46269b024632e5670b8c5
Kaveh Karami <kaveh.karami.kk@gmail.com>
2020-10-11 16:55:31 +0330

    use BlogSidebar Component in Blog Container

--- src/app/containers/Blog.jsx
+++ src/app/containers/Blog.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Article from '../components/blog/Article';
+import BlogSidebar from '../components/blog/BlogSidebar';
 import Pagination from '../components/blog/Pagination';
 import Breadcrumb from '../components/shared/sections/Beadcrumb';
 
@@ -21,81 +22,7 @@ const Blog = () => {
             </div>
 
             <div className="col-lg-4">
-              <div className="sidebar">
-
-                <h3 className="sidebar-title">Search</h3>
-                <div className="sidebar-item search-form">
-                  <form action="">
-                    <input type="text" />
-                    <button type="submit"><i className="icofont-search"></i></button>
-                  </form>
-                </div>
-
-                <h3 className="sidebar-title">Categories</h3>
-                <div className="sidebar-item categories">
-                  <ul>
-                    <li><a href="/blog/#">General <span>(25)</span></a></li>
-                    <li><a href="/blog/#">Lifestyle <span>(12)</span></a></li>
-                    <li><a href="/blog/#">Travel <span>(5)</span></a></li>
-                    <li><a href="/blog/#">Design <span>(22)</span></a></li>
-                    <li><a href="/blog/#">Creative <span>(8)</span></a></li>
-                    <li><a href="/blog/#">Educaion <span>(14)</span></a></li>
-                  </ul>
-
-                </div>
-
-                <h3 className="sidebar-title">Recent Posts</h3>
-                <div className="sidebar-item recent-posts">
-                  <div className="post-item clearfix">
-                    <img src={require("../../assets/img/recent-posts-1.jpg")} alt="" />
-                    <h4><a href="blog-single.html">Nihil blanditiis at in nihil autem</a></h4>
-                    <time dateTime="2020-01-01">Jan 1, 2020</time>
-                  </div>
-
-                  <div className="post-item clearfix">
-                    <img src={require("../../assets/img/recent-posts-2.jpg")} alt="" />
-                    <h4><a href="blog-single.html">Quidem autem et impedit</a></h4>
-                    <time dateTime="2020-01-01">Jan 1, 2020</time>
-                  </div>
-
-                  <div className="post-item clearfix">
-                    <img src={require("../../assets/img/recent-posts-3.jpg")} alt="" />
-                    <h4><a href="blog-single.html">Id quia et et ut maxime similique occaecati ut</a></h4>
-                    <time dateTime="2020-01-01">Jan 1, 2020</time>
-                  </div>
-
-                  <div className="post-item clearfix">
-                    <img src={require("../../assets/img/recent-posts-4.jpg")} alt="" />
-                    <h4><a href="blog-single.html">Laborum corporis quo dara net para</a></h4>
-                    <time dateTime="2020-01-01">Jan 1, 2020</time>
-                  </div>
-
-                  <div className="post-item clearfix">
-                    <img src={require("../../assets/img/recent-posts-5.jpg")} alt="" />
-                    <h4><a href="blog-single.html">Et dolores corrupti quae illo quod dolor</a></h4>
-                    <time dateTime="2020-01-01">Jan 1, 2020</time>
-                  </div>
-                </div>
-
-                <h3 className="sidebar-title">Tags</h3>
-                <div className="sidebar-item tags">
-                  <ul>
-                    <li><a href="/blog/#">App</a></li>
-                    <li><a href="/blog/#">IT</a></li>
-                    <li><a href="/blog/#">Business</a></li>
-                    <li><a href="/blog/#">Business</a></li>
-                    <li><a href="/blog/#">Mac</a></li>
-                    <li><a href="/blog/#">Design</a></li>
-                    <li><a href="/blog/#">Office</a></li>
-                    <li><a href="/blog/#">Creative</a></li>
-                    <li><a href="/blog/#">Studio</a></li>
-                    <li><a href="/blog/#">Smart</a></li>
-                    <li><a href="/blog/#">Tips</a></li>
-                    <li><a href="/blog/#">Marketing</a></li>
-                  </ul>
-
-                </div>
-              </div>
+              <BlogSidebar />
             </div>
           </div>
         </div>
